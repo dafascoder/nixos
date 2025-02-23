1da9cebadff61fa3390cfd5ab8af8eb993b76b09
dafascoder <shirwa256@gmail.com>
2025-02-23 13:12:56 -0500

    setting zsh

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -42,11 +42,8 @@
 
 programs.zsh = {
   enable = true;
-  enableCompletion = true;
-  autosuggestions.enable = true;
-  syntaxHighlighting.enable = true;
 
-  shellAliases = {
+ shellAliases = {
     ll = "ls -l";
     update = "sudo nixos-rebuild switch";
   };
