a5ed3f9af19be94c967ef3f26d22e2087e56bf4c
dafascoder <shirwa256@gmail.com>
2025-02-23 02:51:45 -0500

    adjusting nixos config

--- home-manager/programs/starship/default.nix
+++ home-manager/programs/starship/default.nix
@@ -4,14 +4,14 @@
     enable = true;
     # Configuration written to ~/.config/starship.toml
     settings = {
-      # add_newline = false;
+       add_newline = false;
 
-      # character = {
-      #   success_symbol = "[➜](bold green)";
-      #   error_symbol = "[➜](bold red)";
-      # };
+       character = {
+         success_symbol = "[➜](bold green)";
+         error_symbol = "[➜](bold red)";
+       };
 
-      # package.disabled = true;
+       package.disabled = true;
     };
   };
 }
