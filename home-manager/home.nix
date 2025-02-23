1f57cd7a05e23eb2f39814a9da6e77d2b720b7d0
dafascoder <shirwa256@gmail.com>
2025-02-23 13:12:16 -0500

    setting zsh

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -40,7 +40,18 @@
     };
   };
 
+programs.zsh = {
+  enable = true;
+  enableCompletion = true;
+  autosuggestions.enable = true;
+  syntaxHighlighting.enable = true;
 
+  shellAliases = {
+    ll = "ls -l";
+    update = "sudo nixos-rebuild switch";
+  };
+  history.size = 10000;
+};
 
   programs.ghostty.enable = true;
    
